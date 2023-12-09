9a47c9749894e20d5ed0c6145a5baed323f79998
sid <abubakkar.sideeq@scrapehero.com>
2023-12-09 22:30:04 +0530

    added tests

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from sqlalchemy import create_engine, Column, Integer, String, MetaData, select
 from databases import Database
 from sqlalchemy.ext.declarative import declarative_base
+from PIL import Image
+from io import BytesIO
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
-mongo_client = AsyncIOMotorClient("mongodb://your_username:your_password@mongodb:27017")
+mongo_client = AsyncIOMotorClient("mongodb://username:password@mongodb:27017")
 
-db = mongo_client["mongodb"]
-collection = db["users"]
+db = mongo_client["database"]
+collection = db["Profile"]
 
-DATABASE_URL = "postgresql://your_username:your_password@postgres:5432/your_database"
+
+DATABASE_URL = "postgresql://username:password@postgres:5432/database"
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
@@ -27,7 +30,7 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = "postgres"
+    __tablename__ = "Users"
     id = Column(Integer, primary_key=True, index=True)
     fullname = Column(String, index=True)
     email = Column(String, index=True)
@@ -62,7 +65,9 @@ async def registered(
     phone: str = Form(...),
     profile: UploadFile = File(...),
 ):
-    query = select(User).where(User.email == email)
+
+    query = select(User).where(User.email == email or User.phone == phone)
+
     result = await database.fetch_one(query)
     if result:
         return templates.TemplateResponse(
@@ -70,15 +75,33 @@ async def registered(
             {"request": request, "user_id": result["fullname"], "registered": True},
         )
 
-    # Save to PostgreSQL
+    # Save to PostgresSQL
     query = User.__table__.insert().values(
         fullname=fullname, email=email, password=password, phone=phone
     )
     await database.execute(query)
     # Save to MongoDB
     contents = await profile.read()
-    user_data = {"profile": contents}
+    user_data = {"profile": contents, "email": email}
     await collection.insert_one(user_data)
     return templates.TemplateResponse(
         "success.html", {"request": request, "user_id": fullname}
     )
+
+
+@app.get("/users/", response_class=HTMLResponse)
+async def users(request: Request):
+    query = User.__table__.select()
+    users_result = await database.fetch_all(query)
+
+    # Fetch all user profiles from MongoDB in a single query
+    user_emails = [user.email for user in users_result]
+    profiles_result = await collection.find({"email": {"$in": user_emails}}).to_list(length=len(user_emails))
+
+    # Combine user data and profiles into a single dictionary
+    user_data = {user["email"]: dict(user) for user in users_result}
+    for profile in profiles_result:
+        user_data[profile["email"]]["profile"] = Image.open(BytesIO(profile["profile"])).tobytes()
+    return templates.TemplateResponse("users.html", {"request": request, "users": user_data.values()})
+
+
