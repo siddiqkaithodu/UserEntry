c866004b86576096ab59788e74fe92f0522d40c6
Restyled.io <commits@restyled.io>
2023-12-10 05:12:40 +0000

    Restyled by autopep8

--- main.py
+++ main.py
@@ -72,7 +72,8 @@ async def registered(
     if result:
         return templates.TemplateResponse(
             "success.html",
-            {"request": request, "user_id": result["fullname"], "registered": True},
+            {"request": request,
+                "user_id": result["fullname"], "registered": True},
         )
 
     # Save to PostgresSQL
